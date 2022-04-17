3e6e132f21e2a4d82062c370ce26719806d08263
POLARISAb1 <m18625572652@163.com>
2022-04-17 17:10:43 +0800

    2022-04-17 备份文件

--- deploy-push.sh
+++ deploy-push.sh
@@ -2,9 +2,11 @@ echo "开始执行清楚和编译。。。"
 hexo clean && hexo g
 echo "开始推送博客。。。"
 hexo d
+echo "--- 推送完成 ---"
 
 echo "开始备份文件。。。"
 sj=$(date '+%Y-%m-%d')
 git add .
 git commit -m "$sj 备份文件"
 git push
+echo "--- 备份完成 ---"
